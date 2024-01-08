dbe38a091deff557b5014ea1ac8d778c3dcf6ff8
Andoni1Barba <Andoni1barba@gmail.com>
2024-01-08 13:57:44 -0400

    Components js

---
+++ src/components/Productos.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Productos extends Component{
+    render(){
+        const {productos, agregarAlCarro} = this.props;
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default Productos;
